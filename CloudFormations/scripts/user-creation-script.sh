511583a11cbd2d1849423ca5dbc148e7fac59ba6
daniellcurto <70148832+daniellcurto@users.noreply.github.com>
2020-11-11 20:46:20 -0500

    Update user-creation-script.sh

--- CloudFormations/scripts/user-creation-script.sh
+++ CloudFormations/scripts/user-creation-script.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-for i in 'more new-username-file.txt'
-do
-echo $i
-adduser $i 
-echo "welcomeToFHL" | passwd --stdin "$i"
-done
+if["$UID"!=0]; then
+    for i in 'more new-username-file.txt'
+    do
+    echo $i
+    adduser $i 
+    echo "welcomeToFHL" | passwd --stdin "$i"
+    done
+fi
+
+
 
 chmod 755 new-username-file.txt
 ./newusername.txt
