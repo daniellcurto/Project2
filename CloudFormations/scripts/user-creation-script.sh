1184cf18b8f6a29e56c75aa5ae309530c45597b5
daniellcurto <70148832+daniellcurto@users.noreply.github.com>
2020-11-16 16:28:17 -0500

    Update user-creation-script.sh

--- CloudFormations/scripts/user-creation-script.sh
+++ CloudFormations/scripts/user-creation-script.sh
@@ -6,7 +6,11 @@ fi
 else
     for i in 'more new-username-file.txt'
     do
-    echo '$i" WelcomeToFHL" | passwd --stdin "$i"' << new-username-and-password-file.txt
+    echo $i
+    echo $i |pwgen -y 15| passwd --stdin "$i" >> new-username-and-password-file${date}.txt
+    echo; echo "User $username's password changed!"
+    sudo groupadd -f devops  #the f checks to see if devops exists before creating it
+    echo "Welcome to the FHL family $user" >> /etc/skel/welcome.txt
     done
 fi
 
